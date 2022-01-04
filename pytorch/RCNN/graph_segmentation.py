593b1a57854b712c04ded550179c4cdbc5be562c
Rinat <51479797+Xrenya@users.noreply.github.com>
2022-01-04 16:28:00 +0900

    Create graph_segmentation.py

---
+++ pytorch/RCNN/graph_segmentation.py
@@ -0,0 +1,37 @@
+import skimage
+import skimage.segmentation
+import matplotlib.pyplot as plt
+
+
+def graph_segmentation(img_path: str=None):
+    """This fast 2D image segmentation algorithm
+    Efficient graph-based image segmentation,
+    Felzenszwalb, P.F. and Huttenlocher, D.P.
+    International Journal of Computer Vision, 2004
+    
+    http://people.cs.uchicago.edu/~pff/papers/seg-ijcv.pdf
+
+    :param img_path:
+    :return:
+    """
+    if img_path:
+        img = skimage.io.imread(fname=img_path)
+    else:
+        img = skimage.data.astronaut()
+    # Increasing each parameter will leave only a large area.
+    scales = [100, 300, 100, 100]
+    sigmas = [0.5, 0.5, 0.8, 0.5]
+    min_sizes = [50, 50, 50, 200]
+
+    fig, ax = plt.subplots(2, 2, figsize=(10, 10), sharex=True, sharey=True)
+
+    for i, (scale, sigma, min_size) in enumerate(zip(scales, sigmas, min_sizes)):
+        segments = skimage.segmentation.felzenszwalb(img,
+                                                     scale=scale,
+                                                     sigma=sigma,
+                                                     min_size=min_size)
+        ax[i // 2][i % 2].imshow(skimage.segmentation.mark_boundaries(img, segments))
+        ax[i // 2][i % 2].set_title(f"Scale={scale}, "
+                                    f"sigma={sigma}, "
+                                    f"min_size={min_size}")
+    plt.show()
